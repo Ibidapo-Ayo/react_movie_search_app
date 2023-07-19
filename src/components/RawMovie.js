e1802716b9e08a49967fa415de66c306542c283a
Ibidapo Ayomide Victor <49324541+Ibidapo-Ayo@users.noreply.github.com>
2023-07-19 11:31:33 +0100

    Update RawMovie.js

--- src/components/RawMovie.js
+++ src/components/RawMovie.js
@@ -5,30 +5,30 @@ import "swiper/css";
 import { Navigation } from "swiper";
 
 function RawMovie() {
-  const Movies = [
-    {
-      Title: "Avatar",
-      Year: "2009",
-      Rated: "PG-13",
-      Released: "18 Dec 2009",
-      Runtime: "162 min",
-      Genre: "Action, Adventure, Fantasy",
-      Director: "James Cameron",
-      Writer: "James Cameron",
-      Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
-      Plot:
-        "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-      Language: "English, Spanish",
-      Country: "USA, UK",
-      Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
-      Poster:
-        "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
-      Metascore: "83",
-      imdbRating: "7.9",
-      Images:
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
-    }
-  ];
+  // const Movies = [
+  //   {
+  //     Title: "Avatar",
+  //     Year: "2009",
+  //     Rated: "PG-13",
+  //     Released: "18 Dec 2009",
+  //     Runtime: "162 min",
+  //     Genre: "Action, Adventure, Fantasy",
+  //     Director: "James Cameron",
+  //     Writer: "James Cameron",
+  //     Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+  //     Plot:
+  //       "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+  //     Language: "English, Spanish",
+  //     Country: "USA, UK",
+  //     Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+  //     Poster:
+  //       "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+  //     Metascore: "83",
+  //     imdbRating: "7.9",
+  //     Images:
+  //       "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg"
+  //   }
+  // ];
   return (
     <div>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
